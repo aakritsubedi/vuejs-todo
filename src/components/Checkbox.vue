4a85e2a7d824ad49dde7468e17ac079995b5c66c
Aakrit Subedi <aakritsubedi9@gmail.com>
2020-01-18 11:06:30 +0545

    V-6: Update checkbox functionalities

--- src/components/Checkbox.vue
+++ src/components/Checkbox.vue
@@ -1,6 +1,6 @@
 <template>
     <span class="checkbox" v-bind:class="{'checked':status}" v-on:click="$emit('toggle-status')">
-        <i v-if="status" class="fa fa-check-double"></i>
+        <i v-if="status" class="fa fa-check"></i>
     </span>
 </template>
 
@@ -13,13 +13,14 @@ export default{
 
 <style scoped>
     .checkbox {
-        width: 15px;
-        height: 15px;
+        width: 20px;
+        height: 20px;
         border-radius: 2px;
         display: inline-block;
         border: 1px solid #000000;
     }
     .checked {
-        background-color: #00ff00;
+        border: none;
+        transition: 2s ease-in-out;
     }
 </style>
